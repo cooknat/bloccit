6473bc5dcfcc3649b9d221420538fe655693ec0a
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-07 09:39:57 +0000

    assignment17

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   has_many :comments
+  
+  def self.censoredList
+   Post.all.each do |post| 
+    if post.id % 5 == 1
+     post.update(:title => "SPAM")
+    end
+   end
+  end   
 end
