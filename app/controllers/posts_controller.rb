4d168b90fc5fce88a3f3b5718feffa3255670909
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-07 15:11:35 +0000

    checkpoint18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.censoredList
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,19 +16,28 @@ RSpec.describe PostsController, type: :controller do
        expect(assigns(:posts)).to eq([my_post])
     end
 
- #  describe "GET show" do
- #    it "returns http success" do
- #      get :show
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+  describe "GET show" do
+    it "returns http success" do
+      get :show, params: { id: my_post.id }
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #show view" do
+      get :show, params: { id: my_post.id }
+      expect(response).to render_template :show
+    end
+ 
+    it "assigns my_post to @post" do
+      get :show, params: { id: my_post.id }
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
 
   describe "GET new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
-
   
     it "renders the #new view" do
       get :new
