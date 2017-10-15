93aef5c63246df1a14c2cee974cffeacc48aea10
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-15 18:23:07 +0000

    assignment19

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
   
+  get 'questions/index'
+
+  get 'questions/new'
+
+  get 'questions/create'
+
+  get 'questions/show'
+
+  get 'questions/edit'
+
+  get 'questions/update'
+
+  get 'questions/destroy'
+
   resources :posts
+  
+  resources :questions
 
   get 'about' => 'welcome#about'
   root 'welcome#index'
