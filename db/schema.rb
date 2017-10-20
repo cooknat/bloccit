99f7351a2845d1352c5238d40a6dd6bd17002ede
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-20 12:11:36 +0000

    checkpoint26

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019074434) do
+ActiveRecord::Schema.define(version: 20171020103522) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -26,17 +26,9 @@ ActiveRecord::Schema.define(version: 20171019074434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "topic_id"
+    t.integer "user_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
-  end
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "price"
-    t.integer "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
