7646f7e122d73371451848d7c3149472019e3ae8
Natalie Cook <natalie.t.cook@gmail.com>
2017-10-25 13:07:35 +0000

    assignment27

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,5 +1,8 @@
 module TopicsHelper
-  def user_is_authorized_for_topics?
+  def user_is_authorized_for_topics_create_delete?
     current_user && current_user.admin?
+  end
+  def user_is_authorized_for_topics_update?
+    current_user && current_user.moderator? || current_user.admin?
   end
 end
